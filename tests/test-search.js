aea6801c66d20fe792964df50db10cefb6f41659
Max Gfeller <mg@cyon.ch>
2018-03-22 09:06:53 +0100

    Add test case for promise

--- tests/test-search.js
+++ tests/test-search.js
@@ -3,13 +3,21 @@ var test = require('tape')
 
 var key = 'AIzaSyD1J1tnUlAxa0WxO9-XY6AwsuoBc7Dso1w'
 
-test('basic', function (t) {
+test('basic', (t) => {
   search('jsconf', {
     key: key
   }, function (err, results) {
     t.notOk(err, 'no error')
     t.equals(results.length, 30, '30 results')
-    // results.map(function (r) { console.log(r.title + ': ' + r.link) })
+    t.end()
+  })
+})
+
+test('promise', (t) => {
+  var p = search('jsconf', { key: key })
+  t.ok(p)
+  p.then((results) => {
+    t.equals(results.length, 30, '30 results')
     t.end()
   })
 })
